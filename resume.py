993194bac30a5bcec07fc85add618036d87a1755
Study Material <40472233+jarihant130@users.noreply.github.com>
2023-04-01 20:50:06 +0530

    Updating Navbar Link

--- resume.py
+++ resume.py
@@ -31,14 +31,14 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://www.youtube.com/channel/UCeC088dyJsXK_L1bCHZDcjA" target="_blank">Arihant Jain</a>
+  <a class="navbar-brand" href="#" target="_blank">Arihant Jain</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
