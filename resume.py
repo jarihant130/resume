c8635802c5425f0e8db9b2b103ea7d6b4f32306d
Study Material <40472233+jarihant130@users.noreply.github.com>
2023-04-02 16:03:02 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -4,7 +4,7 @@ from PIL import Image
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+# st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
     
 st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">', unsafe_allow_html=True)
 
